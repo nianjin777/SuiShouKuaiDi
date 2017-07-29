4300594ee2e375a9c6a9bb18e5954243ef548a34
KarlCyan <404290080@qq.com>
2017-07-29 20:09:13 +0800

    修改Activity类型

--- app/src/main/java/com/edu/zucc/wmhxa/kuaishou/activity/login/RegisterActivity.java
+++ app/src/main/java/com/edu/zucc/wmhxa/kuaishou/activity/login/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.edu.zucc.wmhxa.kuaishou.activity.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,7 +16,7 @@ import com.edu.zucc.wmhxa.kuaishou.util.SysApplication;
 import static android.R.id.button1;
 import static android.R.id.closeButton;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends Activity {
 
     private Button regist;
     private EditText reg_et_zhanghao;
@@ -41,22 +42,21 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-
-
     //获取控件
     private void findViewById() {
         regist = (Button) findViewById(R.id.register_bt_return);
-        reg_et_zhanghao=(EditText) findViewById(R.id.reg_et_zhanghao);
-        reg_et_mima=(EditText) findViewById(R.id.reg_et_mima);
-        reg_et_remima=(EditText) findViewById(R.id.reg_et_remima);
-        reg_et_name=(EditText) findViewById(R.id.reg_et_name);
-        reg_rb_sexman=(RadioButton) findViewById(R.id.reg_rb_sexman);
-        ref_rb_sexwomen=(RadioButton) findViewById(R.id.ref_rb_sexwomen);
-        reg_et_idcard=(EditText) findViewById(R.id.reg_et_idcard);
-        reg_et_phone=(EditText) findViewById(R.id.reg_et_phone);
-        reg_et_email=(EditText) findViewById(R.id.reg_et_email);
+        reg_et_zhanghao = (EditText) findViewById(R.id.reg_et_zhanghao);
+        reg_et_mima = (EditText) findViewById(R.id.reg_et_mima);
+        reg_et_remima = (EditText) findViewById(R.id.reg_et_remima);
+        reg_et_name = (EditText) findViewById(R.id.reg_et_name);
+        reg_rb_sexman = (RadioButton) findViewById(R.id.reg_rb_sexman);
+        ref_rb_sexwomen = (RadioButton) findViewById(R.id.ref_rb_sexwomen);
+        reg_et_idcard = (EditText) findViewById(R.id.reg_et_idcard);
+        reg_et_phone = (EditText) findViewById(R.id.reg_et_phone);
+        reg_et_email = (EditText) findViewById(R.id.reg_et_email);
         register_bt_new = (Button) findViewById(R.id.register_bt_new);
     }
+
     //设置监听
     private void setListener() {
         regist.setOnClickListener(new View.OnClickListener() {
